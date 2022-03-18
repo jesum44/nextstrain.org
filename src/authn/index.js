cef047b07f34aa9012f7031a9b4629402ee5965d
Thomas Sibley <tsibley@fredhutch.org>
2022-03-17 23:51:55 -0700

    authn: Share our session cookie across nextstrain.org and subdomains
    By setting the cookie's domain, it changes from a host-only cookie (the
    default, limited to just the host that sets it, e.g. nextstrain.org) to
    a domain-wide cookie (including all levels of subdomains).¹
    
    This will allow sessions from nextstrain.org to be portable to
    next.nextstrain.org and dev.nextstrain.org, and vice versa.  These
    instances already share session storage (a Redis instance) and the
    secret used to secure sessions (SESSION_SECRET).  Using a domain-wide
    cookie was the last missing piece.
    
    Note the security implications of this change, added as a comment for
    maximum visibility.
    
    Related to <https://github.com/nextstrain/nextstrain.org/issues/485> and
    <https://github.com/nextstrain/nextstrain.org/issues/492>.
    
    ¹ I'd been under the impression that a leading dot on the cookie domain,
      e.g. .nextstrain.org, was necessary for this behaviour.  That was the
      case for ages with RFC2109 but has long since been outdated by
      RFC6265.

--- src/authn/index.js
+++ src/authn/index.js
@@ -16,6 +16,29 @@ const utils = require("../utils");
 
 const PRODUCTION = process.env.NODE_ENV === "production";
 
+/* In production, share the cookie across nextstrain.org and all subdomains so
+ * sessions are portable (as long as the session store and secret are also
+ * shared by the deployments).  Otherwise, set a host-only cookie.
+ *
+ * Note that this also means the cookie will be sent to third-party services we
+ * host on subdomains, like docs.nextstrain.org, support.nextstrain.org, and
+ * others.  Although we do "trust" these providers, I still have some
+ * reservations about it as it does increase the surface area for potential
+ * session hijacking via cookie theft.  The big mitigation for me is that this
+ * cookie is already HTTP-only, so JS injections on those providers (which are
+ * much much more likely than server-side injections) can't access the cookie.
+ * The comprehensive, longer term solution (that everyone serious about
+ * security does for essentially this same reason) is to move our internal
+ * services off the user-facing domain (e.g. support.nextstrain-team.org)
+ * and/or accomplish session portability another way (e.g. explicit SSO by
+ * next.nextstrain.org against nextstrain.org as an IdP instead of implicit SSO
+ * via session sharing).
+ *   -trs, 18 March 2022
+ */
+const SESSION_COOKIE_DOMAIN = PRODUCTION
+  ? "nextstrain.org"
+  : undefined;
+
 const SESSION_SECRET = PRODUCTION
   ? process.env.SESSION_SECRET
   : "BAD SECRET FOR DEV ONLY";
@@ -203,6 +226,7 @@ function setup(app) {
       rolling: true,
       store: sessionStore(),
       cookie: {
+        domain: SESSION_COOKIE_DOMAIN,
         httpOnly: true,
         sameSite: "lax",
         secure: PRODUCTION,
